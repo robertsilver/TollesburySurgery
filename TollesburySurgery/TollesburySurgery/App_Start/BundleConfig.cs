5307dec046fdbd2d590564aa4dda1d7c541ca28a
robertsilver <robert.silver@outlook.com>
2017-01-22 18:11:58 +0000

    Baseline
    Site now works as per the demo/when I bought from ThemeForest.  No imgs
    from ThemeForest are being used yet.

--- TollesburySurgery/TollesburySurgery/App_Start/BundleConfig.cs
+++ TollesburySurgery/TollesburySurgery/App_Start/BundleConfig.cs
@@ -12,15 +12,15 @@ namespace TollesburySurgery
                         "~/Scripts/Lib/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryRemaining").Include(
-                        "~/Scripts/Lib/jquery.validate*",
-                        "~/Scripts/Lib/jquery-cookie.min",
-                        "~/Scripts/Lib/jquery.appear.min",
-                        "~/Scripts/Lib/jquery.easing",
-                        "~/Scripts/Lib/jquery.easy-pie-chart.min",
-                        "~/Scripts/Lib/jquery.gmap.min",
-                        "~/Scripts/Lib/jquery.isotope.min",
-                        "~/Scripts/Lib/jquery.lazyload.min",
-                        "~/Scripts/Lib/jquery.magnific-popup.min"));
+                        "~/Scripts/Lib/jquery.validation.min.js",
+                        "~/Scripts/Lib/jquery-cookie.min.js",
+                        "~/Scripts/Lib/jquery.appear.min.js",
+                        "~/Scripts/Lib/jquery.easing.js",
+                        "~/Scripts/Lib/jquery.easy-pie-chart.min.js",
+                        "~/Scripts/Lib/jquery.gmap.min.js",
+                        "~/Scripts/Lib/jquery.isotope.min.js",
+                        "~/Scripts/Lib/jquery.lazyload.min.js",
+                        "~/Scripts/Lib/jquery.magnific-popup.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -32,8 +32,16 @@ namespace TollesburySurgery
                         "~/Scripts/Lib/respond.js",
                         "~/Scripts/Lib/common.min.js",
                         "~/Scripts/Lib/owl.carousel.min.js",
-                        "~/Scripts/Lib/theme.js",
-                        "~/Scripts/Lib/vide.min.js"));
+                        "~/Scripts/Lib/vide.min.js",
+                        "~/Scripts/Lib/view.contact.js",
+                        "~/Scripts/Lib/jquery.themepunch.tools.min.js",
+                        "~/Scripts/Lib/jquery.themepunch.revolution.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/App").Include(
+                       "~/Scripts/App/theme.js", 
+                       "~/Scripts/App/custom.js",
+                       "~/Scripts/App/demo-medical.js",
+                       "~/Scripts/App/theme.init.js"));
 
             //bundles.Add(new StyleBundle("~/Content/css").Include(
             //          "~/Content/bootstrap.css",
@@ -47,22 +55,27 @@ namespace TollesburySurgery
                     "~/Content/VendorCss/simple-line-icons.min.css",
                     "~/Content/VendorCss/owl.carousel.min.css",
                     "~/Content/VendorCss/owl.theme.default.min.css",
-                    "~/Content/VendorCss/magnific-popup.min.css",
-                    "~/Content/VendorCss/settings.css",
-                    "~/Content/VendorCss/layers.css",
-                    "~/Content/VendorCss/navigation.css"));
+                    "~/Content/VendorCss/magnific-popup.min.css"));
 
             bundles.Add(new StyleBundle("~/Theme/css").Include(
-                   "~/Content/theme-elements.css",
-                   "~/Content/theme.css"));
+                   "~/Content/theme.css",
+                   "~/Content/theme-elements.css"));
 
-            bundles.Add(new StyleBundle("~/Skin/css").Include(
-                  "~/Content/skin-medical.css"));
+             bundles.Add(new StyleBundle("~/RsPlugin/css").Include(
+                   "~/Content/VendorCss/rs-plugin/settings.css",
+                   "~/Content/VendorCss/rs-plugin/layers.css",
+                   "~/Content/VendorCss/rs-plugin/navigation.css"));
+
+             bundles.Add(new StyleBundle("~/Skin/css").Include(
+                   "~/Content/skin-medical.css"));
+
+              bundles.Add(new StyleBundle("~/Demo/css").Include(
+                   "~/Content/demo-medical.css"));
 
             bundles.Add(new StyleBundle("~/Custom/css").Include(
                   "~/Content/custom.css"));
 
-            BundleTable.EnableOptimizations = true;
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
